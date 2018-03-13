a4aa84694127001a602b938b22d59626d4705ec5
XUQING LIU <ciwei100000@gmail.com>
2018-03-13 07:34:27 -0700

    change file paths, scripts and update SkipMapper

--- hadoopshell.sh
+++ hadoopshell.sh
@@ -5,6 +5,10 @@
 # SkipMapper.java
 # small
 # stopwords
+
+export HADOOP_CLASSPATH=../lib/lucene-core-7.2.1.jar:../lib/lucene-analyzers-common-7.2.1.jar:$HADOOP_CLASSPATH
+external_libs="../lib/lucene-core-7.2.1.jar,../lib/lucene-analyzers-common-7.2.1.jar"
+
 hdfs dfs -rm -r /input
 hdfs dfs -rm -r /output
 hdfs dfs -mkdir /input
@@ -14,4 +18,5 @@ hdfs dfs -mkdir /skip
 hdfs dfs -put stopwords /skip
 hadoop com.sun.tools.javac.Main *.java
 jar cf WC.jar *.class
-hadoop jar WC.jar StopWordSkipper -skip /skip/stopwords /input/small /output 
+rm *.class
+hadoop jar WC.jar StopWordSkipper -libjars $external_libs -skip /skip/stopwords /input/small /output 
