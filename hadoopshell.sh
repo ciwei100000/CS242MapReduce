906a1ddb1ffdb19885795407287b837e3cfd8ab2
XUQING LIU <ciwei100000@gmail.com>
2018-03-19 01:50:09 -0700

    update running script

--- hadoopshell.sh
+++ hadoopshell.sh
@@ -7,15 +7,20 @@
 # small
 # stopwords
 
-export HADOOP_CLASSPATH=../lib/lucene-core-7.2.1.jar:../lib/lucene-analyzers-common-7.2.1.jar:$HADOOP_CLASSPATH
-external_libs="../lib/lucene-core-7.2.1.jar,../lib/lucene-analyzers-common-7.2.1.jar"
+cd ./Hadoop
 
-hdfs dfs -rm -r /input
-hdfs dfs -rm -r /output
-hdfs dfs -mkdir /input
-hdfs dfs -put TestData /input
+export HADOOP_CLASSPATH=../../lucene-core-7.2.1.jar:../../lucene-analyzers-common-7.2.1.jar:$HADOOP_CLASSPATH
+external_libs="../../lucene-core-7.2.1.jar,../../lucene-analyzers-common-7.2.1.jar"
+
+hdfs dfs -rm -r /xuqing_input
+hdfs dfs -rm -r /xuqing_output
+hdfs dfs -mkdir /xuqing_input
+hdfs dfs -put ../small /xuqing_input
 
 hadoop com.sun.tools.javac.Main *.java
 jar cf WC.jar *.class
 rm *.class
-hadoop jar WC.jar StopWordSkipper -libjars $external_libs /input/TestData /output
+hadoop jar WC.jar SecondarySortDriver -libjars $external_libs /xuqing_input/small /xuqing_output/temp /xuqing_output/final
+cd ../
+hdfs dfs -get /xuqing_output/final/*
+hdfs dfs -rm -r /xuqing_output
